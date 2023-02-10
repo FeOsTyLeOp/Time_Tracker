5322e1aa3ccd6e4b79466c49d99dc992492eec3f
Vitaliy Shpak <mcinf6@mail.ru>
2023-02-10 18:06:57 +0200

    add icon , update Time Tracker

--- Time_tracker.py
+++ Time_tracker.py
@@ -1,10 +1,144 @@
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ListProperty
+
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.icon_definitions import md_icons
+from kivymd.uix.tab import MDTabsBase
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineIconListItem, MDList
+
+KV = '''
+# Menu item in the DrawerList list.
+<ItemDrawer>:
+    theme_text_color: "Custom"
+    on_release: self.parent.set_color_item(self)
+
+    IconLeftWidget:
+        id: icon
+        icon: root.icon
+        theme_text_color: "Custom"
+        text_color: root.text_color
 
 
-class TimeTrackerApp(MDApp):
+<ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
+
+    AnchorLayout:
+        anchor_x: "left"
+        size_hint_y: None
+        height: avatar.height
+
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "112dp", "112dp"
+            source: "data/logo/icon_title.png"
+
+    MDLabel:
+        text: "STL Time Tracker"
+        font_style: "Button"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    MDLabel:
+        text: "feostyleop@gmail.com"
+        font_style: "Caption"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    ScrollView:
+
+        DrawerList:
+            id: md_list
+
+
+
+Screen:
+
+    MDNavigationLayout:
+
+        ScreenManager:
+
+            Screen:
+
+                BoxLayout:
+                    orientation: 'vertical'
+
+                    MDTopAppBar:
+                        title: "STL Time Tracker"
+                        elevation: 2
+                        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+
+                    MDTabs:
+                        id: tabs
+
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                id: content_drawer
+'''
+
+
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
+
+
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty((0, 0, 0, 1))
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+
+class StlTimeTracker(MDApp):
     def build(self):
-        return MDLabel(text="Welcome to Time Tracker", halign="center")
+        self.theme_cls.primary_palette = "Orange"
+        return Builder.load_string(KV)
 
+    def on_start(self):
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
+        #for name_tab in list(md_icons.keys())[15:30]:
+            #self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab))
+        icon_tab_items = {
+            'clock-plus-outline': 'Активности',
+            'playlist-check': 'История',
+            'google-analytics': 'График отслеживания'
 
-TimeTrackerApp().run()
+        }
+        for icon_name,name_tab in icon_tab_items.items():
+            self.root.ids.tabs.add_widget(
+                Tab(icon =icon_name,title =name_tab)
+            )
+
+StlTimeTracker().run()
